050f7c7309ce7aee2480826cf4feea1da897c4be
root <root@pbxdev01.godty.net>
2018-04-22 17:30:48 +0900

    disableCommercialModules.sh を追加

---
+++ disableCommercialModules.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+fwconsole ma remove areminder
+fwconsole ma remove broadcast
+fwconsole ma remove callerid
+fwconsole ma remove calllimit
+fwconsole ma remove conferencespro
+fwconsole ma remove extensionroutes
+fwconsole ma remove pagingpro
+fwconsole ma remove parkpro
+fwconsole ma remove pinsetspro
+fwconsole ma remove pms
+fwconsole ma remove qxact_reports
+fwconsole ma remove recording_report
+fwconsole ma remove restapps
+fwconsole ma remove sipstation
+fwconsole ma remove sms
+fwconsole ma remove vmnotify
+fwconsole ma remove voicemail_report
+fwconsole ma remove vqplus
+fwconsole ma remove faxpro
+fwconsole ma remove sangomacrm
+fwconsole ma remove webcallback
+fwconsole ma remove cos
+fwconsole ma remove sysadmin
+fwconsole ma remove endpoint
+fwconsole ma remove zulu
+
+fwconsole r
